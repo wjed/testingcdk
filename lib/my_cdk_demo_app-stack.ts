1a049bae7cc32058507eef469f40930a488c2d95
Will Jedrzejczak <williamjedrzejczak@outlook.com>
2025-03-19 13:20:12 -0400

    Changes to my AWS CDK demo app stack: -Renamed a constructor parameter

--- lib/my_cdk_demo_app-stack.ts
+++ lib/my_cdk_demo_app-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,16 @@ export class MyCdkDemoAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // L1 and L2 Construct of an S3 Bucket
+    const level1S3bucket = new CfnBucket(this, 'MyFirstLevel1ConstructBucket', {
+      versioningConfiguration: {
+        status: 'Enabled'
+      }
+    }); 
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyCdkDemoAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const level2S3bucket = new Bucket(this, 'MyFirstLevel2ConstructBucket', {
+      bucketName: 'MyFirstLevel2ConstructBucketForRealWJJ',
+      versioned: true
+    });
   }
 }
